affd3e6c5e8885049e68757c7a0805a45037a65b
walid389 <waliddridi389@gmail.com>
2025-01-09 09:05:57 +0100

    add ex1.py

---
+++ ex 1.py
@@ -0,0 +1,18 @@
+
+import sqlite3
+
+conn = sqlite3.connect("ma_base_de_donnees.db")
+
+cursor = conn.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT);")
+cursor.execute("INSERT INTO users (name) VALUES ('walid');")
+conn.commit()
+
+cursor.execute("SELECT * FROM users;")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.close()
+conn.close()
+
